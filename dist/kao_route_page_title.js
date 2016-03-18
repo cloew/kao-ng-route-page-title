fc1c1655b12af981b248d43fcb38f524bc976bd5
Chris Loew <cloew123@gmail.com>
2016-03-18 17:35:35 -0600

    Added ability to specify a standard format for page titles

--- dist/kao_route_page_title.js
+++ dist/kao_route_page_title.js
@@ -1,10 +1,20 @@
 $traceurRuntime.ModuleStore.getAnonymousModule(function() {
   "use strict";
-  angular.module("kao.route.page-title", ["ngRoute", "kao.page-title"]).run(function($rootScope, PageTitle) {
+  angular.module("kao.route.page-title", ["ngRoute", "kao.page-title"]).provider("PageTitleFormat", function() {
+    var format = void 0;
+    this.set = function(templateFormat) {
+      format = templateFormat;
+    };
+    this.$get = function($interpolate) {
+      return {"format": function(route) {
+          return $interpolate(format)(route);
+        }};
+    };
+  }).run(function($rootScope, PageTitle, PageTitleFormat) {
     $rootScope.$on("$routeChangeSuccess", function(event, current, previous) {
-      console.log(current);
       if (current.title) {
-        PageTitle.set(current.title);
+        var title = PageTitleFormat.format(current);
+        PageTitle.set(title);
       }
     });
   });
