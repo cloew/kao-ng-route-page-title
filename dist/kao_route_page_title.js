8ac798ace610d21c00487f3316fc5ec00577dcc7
Chris Loew <cloew123@gmail.com>
2016-03-19 09:37:28 -0600

    Moved the Formatted Page Title to the core page-title repo

--- dist/kao_route_page_title.js
+++ dist/kao_route_page_title.js
@@ -1,20 +1,9 @@
 $traceurRuntime.ModuleStore.getAnonymousModule(function() {
   "use strict";
-  angular.module("kao.route.page-title", ["ngRoute", "kao.page-title"]).provider("PageTitleFormat", function() {
-    var format = void 0;
-    this.set = function(templateFormat) {
-      format = templateFormat;
-    };
-    this.$get = function($interpolate) {
-      return {"format": function(route) {
-          return $interpolate(format)(route);
-        }};
-    };
-  }).run(function($rootScope, PageTitle, PageTitleFormat) {
+  angular.module("kao.route.page-title", ["ngRoute", "kao.page-title"]).run(function($rootScope, PageTitle, FormattedPageTitle) {
     $rootScope.$on("$routeChangeSuccess", function(event, current, previous) {
       if (current.title) {
-        var title = PageTitleFormat.format(current);
-        PageTitle.set(title);
+        FormattedPageTitle.set(current);
       }
     });
   });
