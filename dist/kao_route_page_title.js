8277a8dc1fb179a8d87b0235ab0a59f3e19ff931
Chris Loew <cloew123@gmail.com>
2016-03-19 10:06:24 -0600

    Added ability to specify a full_title which overrides the formatted apge title

--- dist/kao_route_page_title.js
+++ dist/kao_route_page_title.js
@@ -2,8 +2,12 @@ $traceurRuntime.ModuleStore.getAnonymousModule(function() {
   "use strict";
   angular.module("kao.route.page-title", ["ngRoute", "kao.page-title"]).run(function($rootScope, PageTitle, FormattedPageTitle) {
     $rootScope.$on("$routeChangeSuccess", function(event, current, previous) {
-      if (current.title) {
-        FormattedPageTitle.set(current);
+      if (current.full_title) {
+        PageTitle.set(current.full_title, current);
+      } else {
+        if (current.title) {
+          FormattedPageTitle.set(current);
+        }
       }
     });
   });
